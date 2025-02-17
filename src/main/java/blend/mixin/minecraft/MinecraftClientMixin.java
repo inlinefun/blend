3ac2658dd3ba99a8e4dbf1ea70487c9c1a859cc3
inlinefun <twenty48fr@gmail.com>
2025-02-17 20:10:54 +0530

    cleanup: project structure

---
+++ src/main/java/blend/mixin/minecraft/MinecraftClientMixin.java
@@ -0,0 +1,11 @@
+package blend.mixin.minecraft;
+
+import net.minecraft.client.MinecraftClient;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(MinecraftClient.class)
+public class MinecraftClientMixin {
+
+
+
+}
---
+++ src/main/kotlin/blend/Initializer.kt
@@ -0,0 +1,9 @@
+package blend
+
+import net.fabricmc.api.ClientModInitializer
+
+object Initializer: ClientModInitializer {
+	override fun onInitializeClient() {
+
+	}
+}
