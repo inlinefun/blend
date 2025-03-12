c89943748624c4c3c9f10e1fd50006a8410b4cbc
inlinefun <twenty48fr@gmail.com>
2025-03-12 13:57:21 +0530

    Initial commit

---
+++ src/main/kotlin/blend/Client.kt
@@ -0,0 +1,18 @@
+package blend
+
+import kotlinx.datetime.Clock
+
+object Client {
+
+    const val NAME = "Blend"
+    const val VERSION = "5.0"
+
+    fun init() {
+
+    }
+
+    fun shutdown() {
+
+    }
+
+}
---
+++ src/main/kotlin/blend/Initializer.kt
@@ -0,0 +1,7 @@
+package blend
+
+import net.fabricmc.api.ClientModInitializer
+
+class Initializer: ClientModInitializer {
+    override fun onInitializeClient() = Client.init()
+}
